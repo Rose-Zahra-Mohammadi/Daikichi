fb0c6cd2c22dfaca7b9ded5703e33cf0a351bc9e
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-08-08 17:16:03 -0400

    using PathVariables

--- src/main/java/com/rosezahramohammadi/daikichi/DaikichiController.java
+++ src/main/java/com/rosezahramohammadi/daikichi/DaikichiController.java
@@ -1,7 +1,6 @@
 package com.rosezahramohammadi.daikichi;
 
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +13,7 @@ public class DaikichiController {
 	public String daikichi() {
 		return "Welcome";
 	}
-	
+
 	@RequestMapping("/today")
 	public String today() {
 		return "Today you will find luck in all your endeavors";
@@ -25,6 +24,19 @@ public class DaikichiController {
 		return "Tomorrow an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
+	@RequestMapping("/travel/{place}")
+	public String travel(@PathVariable("place") String place) {
+		return "Congratulations! You will soon travel to "+ place;
 	}
-
-
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers";
+		}
+		else {
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	}
+	
+	}
+}
