1bd87da4f1ecf17613f278e817453cdad8300e84
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-08-08 09:36:03 -0400

    Initial

---
+++ src/main/java/com/rosezahramohammadi/daikichi/DaikichiController.java
@@ -0,0 +1,30 @@
+package com.rosezahramohammadi.daikichi;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String daikichi() {
+		return "Welcome";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	}
+
+
